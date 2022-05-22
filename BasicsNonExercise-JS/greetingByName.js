de1fa78a0070136e600d4c2f6b7f96b193456328
Dinkicha <51996162+dinkoproto@users.noreply.github.com>
2022-05-22 17:39:42 +0300

    Add files via upload

---
+++ BasicsNonExercise-JS/greetingByName.js
@@ -0,0 +1,5 @@
+function greeting(input) {
+    let name = input[0];
+    console.log(`Hello, ${name}!`);
+}
+greeting(['Niki']);
---
+++ BasicsNonExercise-JS/petShop.js
@@ -0,0 +1,13 @@
+function pets(input) {
+    let dogFood = 2.50;
+    let catFood = 4;
+
+    let countDogFood = input[0];
+    let countCatFood = input[1];
+
+    console.log(dogFood * countDogFood + catFood * countCatFood);
+}
+pets(["5 ",
+
+"4 "]);
+   
