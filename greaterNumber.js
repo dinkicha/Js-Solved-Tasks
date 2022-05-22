75ebe287f53ded9c236671ad1791e5f38e540c29
Dinkicha <51996162+dinkoproto@users.noreply.github.com>
2022-05-22 17:22:42 +0300

    Add files via upload

---
+++ greaterNumber.js
@@ -0,0 +1,14 @@
+function greater(input) {
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+if(a > b){
+    console.log(a);
+    
+} else {
+    console.log(b);
+}
+
+}
+greater(["5","3"]);
---
+++ passwordGuess.js
@@ -0,0 +1,12 @@
+function password(input) {
+
+   let vhod = (input[0]);
+
+   if(vhod == (["s3cr3t!P@ssw0rd"])){
+    console.log('Welcome')
+   }else{
+   console.log('Wrong password!')
+   }
+
+}
+password(["s3cr3t!P@ssw0rd"])
