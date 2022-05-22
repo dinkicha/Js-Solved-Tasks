3896a0b095a626b45369a7662f460271ee5d4426
Dinkicha <51996162+dinkoproto@users.noreply.github.com>
2022-05-22 17:36:50 +0300

    Delete concatenateData.js

--- concatenateData.js
+++
@@ -1,8 +0,0 @@
-function concatenate(input) {
-    let fName = input[0];
-    let lName = input[1];
-    let age = Number(input[2]);
-    let town = input[3];
-    console.log(`You are ${fName} ${lName}, a ${age}-years old person from ${town}.`);
-}
-concatenate(['Ivan', 'Ivanov', 15, 'Burgas']);
