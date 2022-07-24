adc99e06f8b0ad2d597a6fc30e89658e7874abcf
dinko <dinkoproto2@gmail.com>
2022-07-24 20:57:24 +0300

    Added new modified dayOfWeek task

--- JsArrays/dayOfWeek.js
+++ JsArrays/dayOfWeek.js
@@ -1,29 +1,17 @@
 function dayOfWeek(day) {
-  let days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  if (day === 1) {
-    console.log("Monday");
-  } else if (day === 2) {
-    console.log("Tuesday");
-  } else if (day === 3) {
-    console.log("Wednesday");
-  } else if (day === 4) {
-    console.log("Thursday");
-  } else if (day === 5) {
-    console.log("Friday");
-  } else if (day === 6) {
-    console.log("Saturday");
-  } else if (day === 7) {
-    console.log("Sunday");
-  } else {
+  if (day < 1 || day > 7) {
     console.log("Invalid day!");
+  } else {
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+    console.log(days[day - 1]);
   }
 }
 dayOfWeek(3);
