e646ff17ce8f8bf00395812c5e635a641568f30c
dinko <dinkoproto2@gmail.com>
2022-10-15 16:38:59 +0300

    Test

---
+++ secondLanguage-FirstSteps/01.Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
