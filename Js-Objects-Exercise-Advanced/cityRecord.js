3b9a4049d92ff785b80c90485d2cf758ceb70884
dinko <dinkoproto2@gmail.com>
2023-01-15 19:05:11 +0200

    Adding 3 new tasks

---
+++ Js-Objects-Exercise-Advanced/cityRecord.js
@@ -0,0 +1,13 @@
+function main(cityName, population, treasury) {
+    let obj = {
+        'name': cityName,
+        'population': population,
+        'treasury': treasury
+    }
+    return obj
+}
+console.log(main('Tortuga',
+
+    7000,
+
+    15000));
---
+++ Js-Objects-Exercise-Advanced/cityTaxes.js
@@ -0,0 +1,33 @@
+function cityTaxes(name, population, treasury) {
+    let obj = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * percentage / 100);
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.ceil(this.treasury * (percentage / 100));
+        }
+    }
+    return obj
+}
+const city =
+
+cityTaxes('Tortuga',
+
+7000,
+
+15000);
+
+city.collectTaxes();
+
+console.log(city.treasury);
+
+city.applyGrowth(5);
+
+console.log(city.population)
