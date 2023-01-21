b04ba07cc3ddf0b013bce85cd838041375e47e0e
dinko <dinkoproto2@gmail.com>
2023-01-21 16:34:19 +0200

    Adding one modified task

--- Js-Objects-Exercise-Advanced/rectangle.js
+++ Js-Objects-Exercise-Advanced/rectangle.js
@@ -1,15 +1,13 @@
-function rectangle(width, height, color, calcArea) {
-    const rectangle = {
-        width: Number(width),
-        height: Number(height),
+function rectangle(width, height, color) {
+    return {
+        width,
+        height,
         color: color[0].toUpperCase() + color.slice(1),
         calcArea() {
             return this.width * this.height
         }
     }
-    return rectangle
 }
-
 let rect = rectangle(4, 5, 'red');
 
 console.log(rect.width);
