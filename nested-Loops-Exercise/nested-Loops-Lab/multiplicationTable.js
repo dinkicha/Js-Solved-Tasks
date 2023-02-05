aad122e59a317c1bb47dc4c4e343cb2f24ca9430
dinko <dinkoproto2@gmail.com>
2023-02-05 18:22:39 +0200

    Changing folders

---
+++ nested-Loops-Exercise/nested-Loops-Lab/multiplicationTable.js
@@ -0,0 +1,14 @@
+function multiplicationTable(){
+
+    for (let i = 1; i <= 10; i++) {
+        for (let k = 1; k <= 10; k++) {
+            
+            let diff = i * k;
+        console.log(`${i} * ${k} = ${diff}`);
+    }
+        }
+       
+    }
+
+
+multiplicationTable()
