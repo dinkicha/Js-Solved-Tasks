8aa3b702da50ae9b01b27accbc093ed62dbb376b
Gospodin Gospodinov <51996162+dinkicha@users.noreply.github.com>
2023-02-05 18:26:18 +0200

    Delete unitTesting-js/exerciseLecture directory

--- unitTesting-js/exerciseLecture/05.Notification/app.js
+++
@@ -1,6 +0,0 @@
-function notify(message) {
-    let div = document.getElementById('notification');
-    div.textContent = message;
-    div.style.display = 'block';
-    div.addEventListener('click', () => div.style.display = 'none');
-}
